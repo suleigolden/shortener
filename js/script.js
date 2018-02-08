c51352b87e75bcfea21621ab758d69d1b15ad641
Suleiman Mamman A <suleimamman@gmail.com>
2018-02-08 08:28:11 +0100

    Edit style.css fille (display Backgroung image). And Edit script.js file (ShortURL ajax method created)

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function _(elD) {
+  return document.getElementById(elD);
+}
+function ShortURL() {
+  var hr = new XMLHttpRequest();
+  var url = "controller/send_URLauthentication.php";
+  var link = _("userURL").value;
+  var linkUrl = "SendlinkUrl="+link;
+  hr.open("POST", url, true);
+  hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  hr.onreadystatechange = function() {
+    if (hr.readyState == 4 && hr.status == 200) {
+      var return_data = hr.responseText;
+      _("url_status").innerHTML = "<label style='color:#F00; background-color:#FFF; '>"+return_data+"</label>";
+    }
+  }
+ if(link==null || link=="" || link==" "){
+ _("url_status").innerHTML = "<label style='color:#F00; background-color:#FFF; padding:4px;'>Type in URL.</label>";
+  }else{
+    hr.send(linkUrl);
+  _("url_status").innerHTML =
+    "<label style='color:#5cb85c; margin-top:- 29px;'>Shorting URL.........</label>";
+  }
+}
