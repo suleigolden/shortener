f250c605db2957ddf522663b3e3774f5bde273a1
Suleiman Mamman A <suleimamman@gmail.com>
2018-02-08 12:20:11 +0100

    Creating and ading send_URLauthentication.php file into controller folder that will control authentication request between all the ajax method in the script.js file, and URLauthentication.php file inside the model folder

---
+++ controller/send_URLauthentication.php
@@ -0,0 +1,12 @@
+<?php 
+//including the URLauthentication.php file in order to get access to the User_URL_Link class
+include_once("../model/URLauthentication.php");
+$userRequest = new User_URL_Link();
+
+if(isset($_POST['SendlinkUrl'])){
+ $userRequest ->  short_URL($connection);
+}else{
+  //echo "string";
+}
+
+?>
