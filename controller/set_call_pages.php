3eddbfb7e931cf3a89826c6035bae7002c566008
Suleiman Mamman A <suleimamman@gmail.com>
2018-02-09 16:46:34 +0100

    Edit set_call_pages.php file inside the controller foulder. Add checkShortenerURL function that return the last 6 characters after the last slash of the user URL

--- controller/set_call_pages.php
+++ controller/set_call_pages.php
@@ -3,18 +3,33 @@
 
 class Controller_pages {
 
+function checkShortenerURL(){
+//Get URL content
+$CheckShort = $_SERVER['REQUEST_URI'];
+//Get all characters as string after the last slash (/)
+//$CheckShort = explode('/', $CheckShort, 4)[3];
+$CheckShort = substr($CheckShort, strrpos($CheckShort, '/') + 1);
+//Get the last 6 characters from the string
+	if(strlen($CheckShort) > 6){
+	  $CheckShort = substr($CheckShort, -6);
+	}else{
+	  $CheckShort = $CheckShort;
+	}
+
+	return $CheckShort;
+
+}
 //Function to Navigate to Home page 
 public function HomePage(){
     include_once("view/home.php");
 }
 
 //Function to Redirect to user define URL
-public function set_New_pages($page){
-	
+public function set_New_pages($shortener){
 //Remove the Question mark (?) and  assign .php to the name before Redirecting to the use URL link 
- $page = strtok($page, '?');
- include_once("view/".$page.".php");
-        
+ //$page = strtok($page, '?');
+
+ include_once("view/short.php");      
  }
 
 
